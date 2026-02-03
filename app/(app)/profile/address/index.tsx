dfeec4cc267a46eb8cfac078e675ffe5064ca545
Zaid Rehman <zaidrehmann@gmail.com>
2026-02-03 23:46:12 +0500

    updates

--- app/(app)/profile/address/index.tsx
+++ app/(app)/profile/address/index.tsx
@@ -17,6 +17,7 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+// testing
 const AddressPicker = () => {
     const router = useRouter();
     const [region, setRegion] = useState<any>(null);
